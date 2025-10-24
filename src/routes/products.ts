d055040fa11c819ab03d58f75f043d7aef63d9e9
NasiFried <ricegoreng6@gmail.com>
2025-10-24 15:00:08 +0700

    Create products.ts

---
+++ src/routes/products.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { db } from "../db.js";
+import { v4 as uuidv4 } from "uuid";
+
+export const products = Router();
+
+products.get("/", async (_, res) => {
+  const items = await db.all("SELECT * FROM products");
+  res.json(items);
+});
+
+products.post("/", async (req, res) => {
+  const { name, price, stock, currency } = req.body;
+  const id = uuidv4();
+  await db.run(
+    "INSERT INTO products (id, name, price, currency, stock) VALUES (?,?,?,?,?)",
+    [id, name, price, currency, stock]
+  );
+  res.json({ success: true, id });
+});
